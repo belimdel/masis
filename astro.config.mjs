6efdd02d8e2f8aa053e7fb3688db00dc951bf227
belimdel <74381526+belimdel@users.noreply.github.com>
2026-05-27 18:15:52 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
+
 export default defineConfig({
   site: "https://belimdel.github.io",
   base: "/masis"
-});
 });
